277825a576879dec23d440793f6144b5bb2a56b3
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-08-26 23:26:11 +0300

    in_process_solution

--- task_5_guess_number.py
+++ task_5_guess_number.py
@@ -1,10 +1,20 @@
 """Сделать игру с двумя игроками и бесконечным числом попыток. Сделайте возможность выйти из игры по желанию одного из игроков."""
 
-number = int(input("Введите число: "))
+import math
+
+number = input("Введите число: ")
+
+#print(type(number))
+print(math.isnan(number))
 
 sum_number = 0
-for i in range(0, number + 1):
-    if i <= number + 1:
-        sum_number += i
-        print(i," ",sum_number)
-        i += 1
+if int(number) < 0:
+    print("Нужно было ввести положительное число!")
+elif math.isnan(number):
+    print("Нужно было ввести число!")
+else:
+    for i in range(0, int(number) + 1):
+        if i <= int(number) + 1:
+            sum_number += i
+            print(i," ",sum_number)
+            i += 1
