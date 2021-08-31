78327a0c161c1b72554f77d40ca749ce0d961b14
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-08-31 11:07:04 +0300

    add_change_task_9

--- task_9_creator_python.py
+++ task_9_creator_python.py
@@ -1,8 +1,6 @@
-name = None
+name = input("Кто создатель python?: ")
 
-while True:
-    print("Не верно!")
+while name != "Гвидо":
+    print("Не верно")
     name = input("Кто создатель python?: ")
-    if name ==  "Гвидо":
-        break
 print("Верно!")
