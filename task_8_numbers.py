22f3eeb9768de3d54b1d5a4004f88783f9e2769b
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-08-31 11:08:24 +0300

    add_task_8_numbers

--- task_8_numbers.py
+++ task_8_numbers.py
@@ -5,8 +5,6 @@ def number_0_100():
         print(number)
         number += 1
 
-#number_0_100()
-
 
 # Числовой ряд от 0 до n
 def number_0_n():
@@ -16,7 +14,6 @@ def number_0_n():
         print(number)
         number += 1
 
-#number_0_n() 
 
 # Числовой ряд четных чисел от 0 до n
 def even_number_0_n():
@@ -27,4 +24,20 @@ def even_number_0_n():
             print(number)
         number += 1
 
-even_number_0_n()
+
+# Числовой ряд нечетных чисел от 0 до n через continue
+def odd_number_0_n():
+    number = 0
+    n = int(input("Введите число: "))
+    while number <= n:
+        if number %2 == 0:
+            number += 1
+            continue
+        print(number)
+        number += 1
+
+
+#number_0_100()
+#number_0_n() 
+#even_number_0_n()
+odd_number_0_n()
