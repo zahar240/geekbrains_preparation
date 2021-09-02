4e3fe47e08ae7bbafc6313708f73923b5bb6fdc2
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-09-02 23:43:18 +0300

    add_change_task_5_2

--- task_5_2_guess_number.py
+++ task_5_2_guess_number.py
@@ -1,17 +1,18 @@
 import random
 
 answer = random.randint(0, 100)
-user_answer = int(input("Угадайте число от 1 до 100: "))
 
 try_count = 0
 while try_count <= 6:
+    user_answer = int(input("Угадайте число от 1 до 100: "))
+    try_count += 1
+    print(f'Попытка № {try_count}')
     if user_answer == answer:
         print("Поздравляю, вы угадали!")
+        break
     elif user_answer > answer:
         print("Вы ввели слишком большое число!")
-    elif user_answer < answer:
+    else:
         print("Вы ввели слишком маленькое число!")   
-    try_count += 1
-
+    
 print("Конец игры!")
-#print(answer)
