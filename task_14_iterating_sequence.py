0c1df64a71593b00fde3cb2580adfff8b53e64f0
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-09-02 10:47:54 +0300

    add_task_14_iterating_seguence

---
+++ task_14_iterating_sequence.py
@@ -0,0 +1,12 @@
+friend_name = "Aaaa"
+friends = ["Aaaa", "Wwww", "Xxxx", "Qqqq", "Cccc"]
+roles = ("admin", "quest", "user")
+
+i = 0
+while i < len(friends):
+    friend = friends[i]
+    print(friend)
+    i += 1
+
+for i in friends:
+    print(i)
