fbc9dbecb0c79a519e78593ce73bbd46dfc55402
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-09-02 12:05:27 +0300

    add_change

--- task_14_iterating_sequence.py
+++ task_14_iterating_sequence.py
@@ -2,11 +2,13 @@ friend_name = "Aaaa"
 friends = ["Aaaa", "Wwww", "Xxxx", "Qqqq", "Cccc"]
 roles = ("admin", "quest", "user")
 
+# Итерация через while
 i = 0
 while i < len(friends):
     friend = friends[i]
     print(friend)
     i += 1
 
+# Итерация через for
 for i in friends:
     print(i)
